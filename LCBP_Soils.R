3b338ae4ffde7a3e31a4599d63d53ae2cec6db13
MeganEDuffy <duffy.meganvt@gmail.com>
2025-02-26 14:46:38 -0500

    used upscaler to populate directory

---
+++ LCBP_Soils.R
@@ -0,0 +1,16 @@
+# Megan E Duffy ----
+# University of Vermont Rubenstein School of Environment and Natural Resources
+# megan.duffy@uvm.edu
+# see project Readme.txt for directory stucture
+
+# Directory ----
+setwd(dir = "OneDrive/git-repos/LCBP_Soils/")
+
+# Source functions ----
+source_batch("Functions")
+
+
+
+
+
+
